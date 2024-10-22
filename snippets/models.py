cc95f139dc6e4be89bf925baa49898fea257f2b8
devetechnologies <devetechnologies@gmail.com>
2024-10-22 10:21:54 -0400

    working with the model

--- snippets/models.py
+++ snippets/models.py
@@ -4,7 +4,8 @@ from pygments.styles import get_all_styles
 
 LEXERS = [item for item in get_all_lexers() if item[1]]
 LANGUAGE_CHOICES = sorted([(item[1][0], item[0]) for item in LEXERS])
-STYLE_CHOICES = sorted([(item, item) for item in get_all_styles])
+STYLE_CHOICES = sorted([(item, item) for item in get_all_styles()])
+
 # Create your models here.
 
 
